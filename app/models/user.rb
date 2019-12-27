4bc3f29d3bc0145d5d179ebcc40af0cef6245be3
SergioGmezM <sergiogomez9867@gmail.com>
2019-12-27 13:29:33 +0100

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User
   include Mongoid::Document
+  include ActiveModel::SecurePassword
+
   field :name, type: String
   field :email, type: String
+  field :password_digest, type: String
 
+  before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -10,4 +14,8 @@ class User
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
 end
