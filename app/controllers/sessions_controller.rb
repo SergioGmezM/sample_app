c4cbe2b9aefd9f8175dd93e9aa523088a4c0dc79
SergioGmezM <sergiogomez9867@gmail.com>
2020-01-29 18:34:35 +0100

    Implemented cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     @user = User.where(email: params[:session][:email].downcase).first
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
+      remember @user
       redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User
   field :name, type: String
   field :email, type: String
   field :password_digest, type: String
+  field :remember_digest, type: String
+
+  attr_accessor :remember_token
 
   before_save { email.downcase! }
 
@@ -19,10 +22,33 @@ class User
 
   has_secure_password
 
-  # Returns the hash digest of the given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  class << self
+    # Returns the hash digest of the given string.
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
