c02506a26b99689642602419f2e8faa4ad9947ba
TheBender <sergiogomez9867@gmail.com>
2020-01-29 16:07:46 +0100

    Users now login but there are no test for login success yet

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,21 @@
+module SessionsHelper
+
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # Returns the current logged-in user (if any).
+  def current_user
+
+    # Prevents the app from raising an error when the ObjectID doesn't exist
+    if (user_id = session[:user_id]) && !User.where(id: user_id["$oid"]).empty?
+      @current_user ||= User.find(id: user_id)
+    end
+  end
+
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+end
