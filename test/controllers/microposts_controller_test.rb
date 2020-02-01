c6394edff51681647f7eee73492b83c3af2b46b5
SergioGmezM <sergiogomez9867@gmail.com>
2020-02-01 19:26:32 +0100

    Added controller for microposts

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar",
+                     activated: true, activated_at: Time.zone.now)
+
+    @micropost = @user.microposts.create(content: "Lorem ipsum",
+                                          created_at: 15.minutes.ago)
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "Lorem ipsum" } }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@micropost)
+    end
+    assert_redirected_to login_url
+  end
+
+  def teardown
+    @user.destroy
+  end
 end
