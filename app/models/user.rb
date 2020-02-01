99413a21c6e1ba7f29d9afbef282cb780d78bfca
SergioGmezM <sergiogomez9867@gmail.com>
2020-02-01 17:12:55 +0100

    Added password reset mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,10 @@ class User
   field :activated,	type: Boolean, default: false
   field :activation_digest, type: String
   field :activated_at, type: Time
+  field :reset_digest, type: String
+  field :reset_sent_at, type: Time
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -69,6 +71,18 @@ class User
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   private
