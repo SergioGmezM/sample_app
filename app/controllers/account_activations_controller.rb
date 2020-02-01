062e9c50ab0c8bc3c2b86f6de2be37c7619a77d1
SergioGmezM <sergiogomez9867@gmail.com>
2020-02-01 13:19:35 +0100

    Added methods to activate an user's account for User model

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,14 +1,13 @@
 class AccountActivationsController < ApplicationController
 
   def edit
-    
+
     unless User.where(email: params[:email]).count == 0
         user = User.find_by(email: params[:email])
     end
 
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.update_attribute(:activated,    true)
-      user.update_attribute(:activated_at, Time.zone.now)
+      user.activate
       log_in user
       flash[:success] = "Account activated!"
       redirect_to user
