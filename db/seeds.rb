d0d13cfd3da4054712b9827bf5583935c0e1b485
SergioGmezM <sergiogomez9867@gmail.com>
2020-02-01 20:01:48 +0100

    Fixed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ User.create!(name:  "Sergio",
              activated_at: Time.zone.now)
 end
 
-users = User.order(:created_at).take(6)
+users = User.order([:created_at, :asc]).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
