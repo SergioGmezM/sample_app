81a257b3f01db1febea1a89fd6583380a78afc7d
SergioGmezM <sergiogomez9867@gmail.com>
2020-02-02 18:42:16 +0100

    Added tests for follow/unfollow buttons

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -12,7 +12,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
 
     @user.follow(@other_user)
     @other_user.follow(@user)
-    log_in_as(@user)
+    log_in_as(@user, password: "foobar")
   end
 
   test "following page" do
@@ -30,6 +30,34 @@ class FollowingTest < ActionDispatch::IntegrationTest
     assert_match @user.followers.count.to_s, response.body
     @user.followers.each do |user|
       assert_select "a[href=?]", user_path(user)
+    end
+  end
+
+  test "should follow a user the standard way" do
+    assert_difference '@user.active_relationships.count', 1 do
+      post relationships_path, params: { followed_id: @other_user.id }
+    end
+  end
+
+  test "should follow a user with Ajax" do
+    assert_difference '@user.active_relationships.count', 1 do
+      post relationships_path, xhr: true, params: { followed_id: @other_user.id }
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    @user.follow(@other_user)
+    relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
+    assert_difference '@user.active_relationships.count', -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+  test "should unfollow a user with Ajax" do
+    @user.follow(@other_user)
+    relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
+    assert_difference '@user.active_relationships.count', -1 do
+      delete relationship_path(relationship), xhr: true
     end
   end
 
